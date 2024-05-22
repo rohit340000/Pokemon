123f423c1ee741f4d15a9689af755ecfa30f71e4
rohit995049 <161129526+rohit995049@users.noreply.github.com>
2024-05-22 07:44:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,4 +12,10 @@ class Config(object):
     API_ID = int(os.environ["API_ID", 27498866]
     API_HASH = os.environ["API_HASH", "96fbb6ad2e11ab04e83ca09ef3f42455"]
     AUTH_USERS = "6488911325"""
+
+    AUTH_USERS = os.environ.get("AUTH_USERS", "6488911325").split(",")
+    for i in range(len(AUTH_USERS)):
+        AUTH_USERS[i] = int(AUTH_USERS[i])
+
+
 # jisko bhi is repo ko deploy karne me error aa raha ho ( https://t.me/Stubborn1223 ) ye mere TG ka id ha message karke iska screen shot send kar dena me error solve kar dunga. 
