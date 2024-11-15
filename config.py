ded7bbffcb37556fe7e10de57212b422f6f15b06
Pokemon <161129526+rohit995049@users.noreply.github.com>
2024-11-15 10:18:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7586840704:AAGFA8yCvAJqEhTxz_cNYL7bGV48HmsTiJc")
-    API_ID = int(os.environ["API_ID", 25891183]
-    API_HASH = os.environ["API_HASH", "36709c81d7609a81f86de931cbc87f3a"]
-    AUTH_USERS = "6674754789"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ["API_ID", 28921783]
+    API_HASH = os.environ["API_HASH", "36809c81d5609a81f86de671cbc87f3a"]
+    AUTH_USERS = "8265725628"""
 
 
 # jisko bhi is repo ko deploy karne me error aa raha ho ( https://t.me/IamTgRK ) ye mere TG ka id ha message karke iska screen shot send kar dena me error solve kar dunga. 
